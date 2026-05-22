6254fce9832b0abda0661b84a235e3990a0a8e5f
Codenames-Ren <renmizuki38@gmail.com>
2026-05-22 13:20:50 +0700

    register middleware and add custom return for error code 401 and 403

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,10 +1,12 @@
 <?php
 
+use Symfony\Component\HttpFoundation\Response;
+use Spatie\Permission\Exceptions\UnauthorizedException;
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Application;
-
 use Illuminate\Foundation\Configuration\Exceptions;
-
 use Illuminate\Foundation\Configuration\Middleware;
+use Spatie\Permission\Middleware\RoleMiddleware;
 
 return Application::configure(
     basePath: dirname(__DIR__)
@@ -22,12 +24,37 @@ return Application::configure(
 
     ->withMiddleware(function (Middleware $middleware): void {
 
-        //
+        $middleware->alias([
+            'role' => RoleMiddleware::class,
+        ]);
     })
 
     ->withExceptions(function (Exceptions $exceptions): void {
 
-        //
+        $exceptions->render(function (
+            AuthenticationException $e,
+            $request
+        ) {
+
+            return response()->json([
+                'success' => false,
+
+                'message' => 'Unauthenticated'
+            ], Response::HTTP_UNAUTHORIZED);
+        });
+
+        //Forbidden Access Message
+        $exceptions->render(function (
+            UnauthorizedException $e,
+            $request
+        ) {
+
+            return response()->json([
+                'success' => false,
+
+                'message' => 'Access Forbidden'
+            ], Response::HTTP_FORBIDDEN);
+        });
     })
 
     ->create();
