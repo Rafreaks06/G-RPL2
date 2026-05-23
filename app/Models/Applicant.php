e437cade25540676a9bf30a52b3afa0a69d252ec
Codenames-Ren <renmizuki38@gmail.com>
2026-05-23 17:37:45 +0700

    add applicant models'

---
+++ app/Models/Applicant.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Applicant extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'nik',
+        'phone',
+        'address',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(
+            User::class
+        );
+    }
+}
