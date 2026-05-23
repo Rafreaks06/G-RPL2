2351a0800f5e01f4e1420e899f0526181d9d1ffa
Codenames-Ren <renmizuki38@gmail.com>
2026-05-23 16:07:00 +0700

    add course model

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -2,9 +2,50 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Course extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'study_program_id',
+        'code',
+        'name',
+        'semester',
+        'sks',
+        'rpl_type',
+        'status',
+        'is_active',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'is_active' => 'boolean',
+        ];
+    }
+
+    /*
+    | Relations
+    */
+
+    public function studyPrograms()
+    {
+        return $this->belongsToMany(
+            StudyProgram::class
+        );
+    }
+
+    /*
+    | Accessors
+    */
+
+    public function getStatusAttribute(): string
+    {
+        return $this->is_active
+            ? 'active'
+            : 'inactive';
+    }
 }
