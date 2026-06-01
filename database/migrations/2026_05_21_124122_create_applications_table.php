f9ba5e4c6654907a8c17745e34b3956ef96d6be3
Codenames-Ren <renmizuki38@gmail.com>
2026-06-01 14:54:00 +0700

    create migration application

--- database/migrations/2026_05_21_124122_create_applications_table.php
+++ database/migrations/2026_05_21_124122_create_applications_table.php
@@ -12,7 +12,66 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('applications', function (Blueprint $table) {
+
             $table->id();
+
+            /*
+            | Applicant
+            */
+
+            $table->foreignId('applicant_id')
+                ->constrained()
+                ->cascadeOnDelete();
+
+            /*
+            | Target Study Program
+            */
+
+            $table->foreignId('study_program_id')
+                ->constrained()
+                ->cascadeOnDelete();
+
+            /*
+            | Application Information
+            */
+
+            $table->string('application_number')
+                ->unique();
+
+            $table->enum('rpl_type', [
+                'a1',
+                'a2',
+                'hybrid',
+            ]);
+
+            $table->enum('status', [
+                'draft',
+                'submitted',
+                'under_review',
+                'returned',
+                'under_assessment',
+                'assessed',
+                'approved',
+                'rejected',
+            ])->default('draft');
+
+            /*
+            | Review Information
+            */
+
+            $table->text('review_notes')
+                ->nullable();
+
+            $table->unsignedInteger('revision_count')
+                ->default(0);
+
+            /*
+            | Submission
+            */
+
+            $table->timestamp('submitted_at')
+                ->nullable();
+
             $table->timestamps();
         });
     }
