773fd57328bb9823b253107a3f6fd8a931f17563
Codenames-Ren <renmizuki38@gmail.com>
2026-06-01 14:58:28 +0700

    create application model

--- app/Models/Application.php
+++ app/Models/Application.php
@@ -3,8 +3,81 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Application extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+
+        'application_number',
+
+        'applicant_id',
+
+        'study_program_id',
+
+        'rpl_type',
+
+        'status',
+
+        'review_notes',
+
+        'revision_count',
+
+        'submitted_at',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+
+            'revision_count'
+                => 'integer',
+
+            'submitted_at'
+                => 'datetime',
+        ];
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    | Relationships
+    |--------------------------------------------------------------------------
+    */
+
+    public function applicant()
+    {
+        return $this->belongsTo(
+            Applicant::class
+        );
+    }
+
+    public function studyProgram()
+    {
+        return $this->belongsTo(
+            StudyProgram::class
+        );
+    }
+
+    public function a1Courses()
+    {
+        return $this->hasMany(
+            ApplicationA1Course::class
+        );
+    }
+
+    public function a2LearningExperiences()
+    {
+        return $this->hasMany(
+            ApplicationA2LearningExperience::class
+        );
+    }
+
+    public function documents()
+    {
+        return $this->hasMany(
+            ApplicationDocument::class
+        );
+    }
 }
